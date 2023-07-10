dd4e0be48eacebb1ff9ee142cea1cc2068e26ed5
BihikovDev <BihikovDev@gmail.com>
2023-07-10 14:33:22 +0200

    lastCommit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 // SELECTEURS
 let resetBtn = document.querySelector("#reset");
-let scoreJoueur = document.querySelector("#score-joueur");
-let scoreOrdinateur = document.querySelector("#score-ordinateur");
-let btnJoueur = [...document.getElementsByClassName("btn-joueur")];
+let scorePlayer = document.querySelector("#score-joueur");
+let scoreComputer = document.querySelector("#score-ordinateur");
+let btnPlayer = [...document.getElementsByClassName("btn-joueur")];
 let oBulbizarreBtn = document.querySelector("#obulbizarre");
 let oCarapuceBtn = document.querySelector("#ocarapuce");
 let oSalamecheBtn = document.querySelector("#osalameche");
 let message = document.querySelector("#message");
 let nextBtn = document.querySelector("#next");
-let pourcentageVictoire = document.querySelector("#pourcentage-victoire");
-let victoiresUtilisateur = 0;
-let victoiresOrdinateur = 0;
-let elementAudio = document.querySelector('#pokemonSound')
+let winrate = document.querySelector("#pourcentage-victoire");
+let usersWin = 0;
+let computersWin = 0;
+let gameWin = 0;
+let elementAudio = document.querySelector('#pokemonSound');
 
 // FONCTIONS
 
@@ -20,130 +21,156 @@ elementAudio.volume = 0.02;
 
 // Variable pour lancer la manche et désactiver le bouton ensuite
 const jouerManche = (e) => {
-    let choix = e.target.closest(".btn-joueur");
+  let choix = e.target.closest(".btn-joueur");
 
-    btnJoueur.forEach((btn) => {
-        btn.classList.add("desactivated");
-        btn.removeEventListener("click", jouerManche);
-    });
+  btnPlayer.forEach((btn) => {
+    btn.classList.add("desactivated");
+    btn.removeEventListener("click", jouerManche);
+  });
 
-    choix.classList.remove("desactivated");
-    choix.classList.add("active");
+  choix.classList.remove("desactivated");
+  choix.classList.add("active");
 
-    let choixJoueur = choix.id;
+  let choixJoueur = choix.id;
 
-    let choixOrdi = faireChoixOrdinateur();
+  let choixOrdi = faireChoixOrdinateur();
 
-    verifierGagnant(choixJoueur, choixOrdi);
+  verifierGagnant(choixJoueur, choixOrdi);
 
-    nextBtn.style.visibility = "visible";
+  nextBtn.style.visibility = "visible";
 };
+
 // Fonction choix aléatoire pour l'ordinateur
 const faireChoixOrdinateur = () => {
-    let nbAleatoire = Math.floor(Math.random() * 3);
+  let nbAleatoire = Math.floor(Math.random() * 3);
 
-    switch (nbAleatoire) {
-        case 0:
-            oBulbizarreBtn.classList.add("active");
-            return bulbizarre;
-        case 1:
-            oCarapuceBtn.classList.add("active");
-            return carapuce;
-        default:
-            oSalamecheBtn.classList.add("active");
-            return salameche;
-    }
+  switch (nbAleatoire) {
+    case 0:
+      oBulbizarreBtn.classList.add("active");
+      return bulbizarre;
+    case 1:
+      oCarapuceBtn.classList.add("active");
+      return carapuce;
+    default:
+      oSalamecheBtn.classList.add("active");
+      return salameche;
+  }
 };
-// Déclaration des valeurs de chaque possibilités
+
+// Déclaration des valeurs de chaque possibilité
 const bulbizarre = "bulbizarre";
 const carapuce = "carapuce";
 const salameche = "salameche";
+
 // Fonction pour vérifier le vainqueur et retourner un message selon le résultat
 const verifierGagnant = (choixJoueur, choixOrdi) => {
-    if (choixJoueur == choixOrdi) {
-        message.innerHTML = "<span style='color:orange; font-size:30px'>Match nul, vos deux pokémons sont hors-jeu.</span>"
-        return;
+  if (choixJoueur === choixOrdi) {
+    message.innerHTML = "<span style='color:orange; font-size:30px'>Match nul, vos deux pokémons sont hors-jeu.</span>";
+    return;
+  }
+
+  // Bulbizarre bat Carapuce, mais perd face à Salamèche
+  if (choixJoueur === bulbizarre) {
+    if (choixOrdi === carapuce) {
+      return victoireJoueur();
+    } else if (choixOrdi === salameche) {
+      return victoireOrdinateur();
     }
-// Bulbizarre bat Carapuce, mais perd face à Salamèche
-    if (choixJoueur == bulbizarre) {
-        if (choixOrdi == carapuce) {
-            return victoireJoueur();
-        } else if (choixOrdi == salameche) {
-            return victoireOrdinateur();
-        }
+  }
+
+  // Carapuce bat Salamèche, mais perd face à Bulbizarre
+  if (choixJoueur === carapuce) {
+    if (choixOrdi === salameche) {
+      return victoireJoueur();
+    } else if (choixOrdi === bulbizarre) {
+      return victoireOrdinateur();
     }
-// Carapuce bat Salamèche, mais perd face à Bulbizarre
-    if (choixJoueur == carapuce) {
-        if (choixOrdi == salameche) {
-            return victoireJoueur();
-        } else if (choixOrdi == bulbizarre) {
-            return victoireOrdinateur();
-        }
+  }
+
+  // Salamèche bat Bulbizarre, mais perd face à Carapuce
+  if (choixJoueur === salameche) {
+    if (choixOrdi === bulbizarre) {
+      return victoireJoueur();
+    } else if (choixOrdi === carapuce) {
+      return victoireOrdinateur();
     }
-// Salamèche bat Bulbizarre, mais perd face à Carapuce
-    if (choixJoueur == salameche) {
-        if (choixOrdi == bulbizarre) {
-            return victoireJoueur();
-        } else if (choixOrdi == carapuce) {
-            return victoireOrdinateur();
-        }
-    }
+  }
 };
+
 // En cas de victoire de l'ordinateur, le message.textContent est affiché sous le bouton
 const victoireOrdinateur = () => {
-    message.innerHTML = "<div class='winOrLooseMsg'><span style='color:#ff6565; font-size:30px' class='mb-3'>Vous avez perdu. Vous êtes hors jeu.</span> <img src=\"https://s12.gifyu.com/images/SWwGw.gif\" width=\"100%\" height=\"300px\"></div>"
-    scoreOrdinateur.textContent++;
-    victoiresOrdinateur++;
-    calculerPourcentageVictoire();
+  message.innerHTML = "<div class='winOrLooseMsg'><span style='color:#ff6565; font-size:30px' class='mb-3'>Vous avez perdu. Vous êtes hors jeu.</span> <img src=\"https://s12.gifyu.com/images/SWwGw.gif\" width=\"100%\" height=\"300px\"></div>";
+  scoreComputer.textContent++;
+  computersWin++;
+  if (computersWin=== 5) {
+    displayLooserGif();
+  }
+  calculateWinrate();
 };
+
 // En cas de victoire de l'utilisateur, le message.textContent est affiché sous le bouton
 const victoireJoueur = () => {
-    message.innerHTML = "<div class='winOrLooseMsg'><span style='color:#90EE90; font-size:30px' class='mb-3'>Vous avez gagné. Votre adversaire est hors jeu.</span> <img src=\"https://media.tenor.com/7Nj-xYFfoi8AAAAd/pokemon-pocket-monsters.gif\" width=\"100%\" height=\"300px\"></div>"
-    scoreJoueur.textContent++;
-    victoiresUtilisateur++;
-    calculerPourcentageVictoire();
+  message.innerHTML = "<div class='winOrLooseMsg'><span style='color:#90EE90; font-size:30px' class='mb-3'>Vous avez gagné. Votre adversaire est hors jeu.</span> <img src=\"https://media.tenor.com/7Nj-xYFfoi8AAAAd/pokemon-pocket-monsters.gif\" width=\"100%\" height=\"300px\"></div>";
+  scorePlayer.textContent++;
+  usersWin++;
+  gameWin++;
+  if (usersWin === 5) {
+    displayFullscreen();
+  }
+  calculateWinrate();
 };
+
 // Fonction pour pouvoir relancer une manche
 const preparerNouvelleManche = () => {
-    btnJoueur.forEach((btn) => {
-        btn.classList.remove("desactivated");
-        btn.classList.remove("active");
-        btn.addEventListener("click", jouerManche);
-    });
-// Cache le bouton lorsque le choix est effectué et validé
-    nextBtn.style.visibility = "hidden";
+  btnPlayer.forEach((btn) => {
+    btn.classList.remove("desactivated");
+    btn.classList.remove("active");
+    btn.addEventListener("click", jouerManche);
+  });
 
-    oBulbizarreBtn.classList.remove("active");
-    oCarapuceBtn.classList.remove("active");
-    oSalamecheBtn.classList.remove("active");
+  // Cache le bouton lorsque le choix est effectué et validé
+  nextBtn.style.visibility = "hidden";
 
-    message.textContent = "A vous de jouer !";
+  oBulbizarreBtn.classList.remove("active");
+  oCarapuceBtn.classList.remove("active");
+  oSalamecheBtn.classList.remove("active");
+
+  message.textContent = "A vous de jouer !";
 };
+
 // Ajout d'un événement permettant de réinitialiser les stats et recommencer une partie
 resetBtn.addEventListener("click", () => {
-    scoreJoueur.textContent = 0;
-    scoreOrdinateur.textContent = 0;
-    pourcentageVictoire.textContent = "0%";
+  scorePlayer.textContent = 0;
+  scoreComputer.textContent = 0;
+  winrate.textContent = "0%";
 
-    preparerNouvelleManche();
+  preparerNouvelleManche();
 });
 
 nextBtn.addEventListener("click", preparerNouvelleManche);
 
-btnJoueur.forEach((btn) => btn.addEventListener("click", jouerManche));
-
+btnPlayer.forEach((btn) => btn.addEventListener("click", jouerManche));
 
 // AFFICHAGE IMAGE PLEIN ECRAN
-
+const displayFullscreen = () => {
+  const fullscreenElement = document.querySelector("#fullscreen");
+  fullscreenElement.classList.add("fullscreen-gif");
+  fullscreenElement.style.backgroundImage = "url(https://64.media.tumblr.com/f566549a6cc6295b682fa26ce1606f8f/ddb594a3ece418dc-e1/s540x810/4898348b15ce6adc550b7de45bb2b3b3e1d47b75.gif)";
+};
+const displayLooserGif = () => {
+    const fullscreenLoose = document.querySelector("#fullscreen");
+    fullscreenLoose.classList.add("fullscreen-gif");
+    fullscreenLoose.style.backgroundImage = "url(https://media.tenor.com/7C6H6TQk-D8AAAAC/pokemon-ash.gif)";
+};
 
 // Fonction pour calculer le pourcentage de victoire de l'utilisateur
-const calculerPourcentageVictoire = () => {
-  let totalVictoires = victoiresUtilisateur + victoiresOrdinateur;
+const calculateWinrate = () => {
+  let totalVictoires = usersWin + computersWin;
   if (totalVictoires === 0) {
-    pourcentageVictoire.textContent = "0%";
+    winrate.textContent = "0%";
   } else {
-    let pourcentage = (victoiresUtilisateur / totalVictoires) * 100;
-    pourcentageVictoire.textContent = `${pourcentage.toFixed(2)}%`;
+    let pourcentage = (usersWin / totalVictoires) * 100;
+    winrate.textContent = `${pourcentage.toFixed(2)}%`;
   }
 };
-calculerPourcentageVictoire()
+calculateWinrate()
